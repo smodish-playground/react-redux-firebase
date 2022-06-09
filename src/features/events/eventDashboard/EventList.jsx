981e32596c1c90884c5eb6607c7366574520a12c
Steve Smodish <smodish@hotmail.com>
2022-06-09 17:19:03 -0400

    layout functional

---
+++ src/features/events/eventDashboard/EventList.jsx
@@ -0,0 +1,11 @@
+import EventListItem from './EventListItem'
+
+export default function EventList({ events }) {
+  return (
+    <>
+      {events.map((event) => (
+        <EventListItem event={event} key={event.id} />
+      ))}
+    </>
+  )
+}
--- src/index.js
+++ src/index.js
@@ -1,7 +1,27 @@
 import React from 'react'
-import ReactDOM from 'react-dom/client'
+import ReactDOM from 'react-dom'
+import 'semantic-ui-css/semantic.min.css'
+import 'react-toastify/dist/ReactToastify.min.css'
+import 'react-calendar/dist/Calendar.css'
+import './app/layout/styles.css'
 import App from './app/layout/App'
+import * as serviceWorker from './serviceWorker'
 
-const root = ReactDOM.createRoot(document.getElementById('root'))
+const rootEl = document.getElementById('root')
 
-root.render(<App />)
+function render() {
+  ReactDOM.render(<App />, rootEl)
+}
+
+if (module.hot) {
+  module.hot.accept('./app/layout/App', function () {
+    setTimeout(render)
+  })
+}
+
+render()
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.register()
