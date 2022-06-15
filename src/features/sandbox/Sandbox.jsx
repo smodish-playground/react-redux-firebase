b7b25a0cb889a56264408b56423c62b210f14d75
Steve Smodish <smodish@hotmail.com>
2022-06-15 15:03:20 -0400

    integrate react-places-autocomplete hook up to google apis create test component

--- src/features/sandbox/Sandbox.jsx
+++ src/features/sandbox/Sandbox.jsx
@@ -3,6 +3,7 @@ import { useDispatch, useSelector } from 'react-redux'
 import { Button } from 'semantic-ui-react'
 import { increment, decrement } from './testReducer'
 import { openModal } from '../../app/common/modals/modalReducer'
+import TestPlaceInput from './TestPlaceInput'
 
 export default function Sandbox() {
   const dispatch = useDispatch()
@@ -29,6 +30,9 @@ export default function Sandbox() {
           dispatch(openModal({ modalType: 'TestModal', modalProps: { data } }))
         }
       />
+      <div style={{ marginTop: 15 }}>
+        <TestPlaceInput />
+      </div>
     </>
   )
 }
