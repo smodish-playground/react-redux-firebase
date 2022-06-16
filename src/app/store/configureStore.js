44ea951b4ac8ef9a730400017a60edfe9895c412
Steve Smodish <smodish@hotmail.com>
2022-06-16 13:12:36 -0400

    install redux-thunk and setup async reducer

--- src/app/store/configureStore.js
+++ src/app/store/configureStore.js
@@ -1,8 +1,10 @@
+import { applyMiddleware } from 'redux'
 import { createStore } from 'redux'
-import { devToolsEnhancer } from 'redux-devtools-extension'
+import { composeWithDevTools } from 'redux-devtools-extension'
+import thunk from 'redux-thunk'
 
 import rootReducer from './rootReducer'
 
 export function configureStore() {
-  return createStore(rootReducer, devToolsEnhancer())
+  return createStore(rootReducer, composeWithDevTools(applyMiddleware(thunk)))
 }
